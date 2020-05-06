8f7e36f265ea1987672282dcef0a838615cd60aa
Hyunseung Jeon <dogdriip@gmail.com>
2020-05-06 14:22:33 +0900

    mod LoginActivity.kt

--- giggle_android/app/src/main/java/com/giggle/prototype/LoginActivity.kt
+++ giggle_android/app/src/main/java/com/giggle/prototype/LoginActivity.kt
@@ -17,29 +17,36 @@ class LoginActivity : AppCompatActivity() {
 
         //로그인
         login.setOnClickListener {
-            if(email.text.toString().isEmpty() || password.text.toString().isEmpty()){
-                Toast.makeText(this,"email 혹은 password를 반드시 입력하세요.",Toast.LENGTH_SHORT).show()
-            } else{
-                auth?.signInWithEmailAndPassword(email.text.toString(),password.text.toString())?.addOnCompleteListener(this) { task ->
-                    if(task.isSuccessful){
-                        var useremailveri =auth?.currentUser?.isEmailVerified
-                        if(useremailveri == true){
+            val emailStr: String = email.text.toString()
+            val passwordStr: String = password.text.toString()
+
+            if (emailStr.isEmpty()) {
+                Toast.makeText(this,"이메일을 입력해 주세요.", Toast.LENGTH_SHORT).show()
+            } else if (passwordStr.isEmpty()) {
+                Toast.makeText(this,"비밀번호를 입력해 주세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                auth?.signInWithEmailAndPassword(emailStr, passwordStr)?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val emailVerified = auth?.currentUser?.isEmailVerified
+                        if (emailVerified == true){
                             val nextIntent = Intent(this, SignInActivity::class.java)
                             startActivity(nextIntent)
-                        }else {
-                            Toast.makeText(this, "이메일 인증을 완료해주세요", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this, "이메일 인증을 완료해주세요.", Toast.LENGTH_SHORT).show()
                         }
-                    }else{
-                        Toast.makeText(this, "email 혹은 password를 확인 하십시요.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "이메일 혹은 비밀번호를 확인하세요.", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
         }
+
         //회원가입
         signup.setOnClickListener {
             val nextIntent = Intent(this, SignUpActivity1::class.java)
             startActivity(nextIntent)
         }
+
         //비밀번호 재설정
         resetpassword.setOnClickListener {
             val nextIntent = Intent(this, ResetActivity::class.java)
