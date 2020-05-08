e7b265e9b38aabad16f31e36fd693752bafe2f22
Hyunseung Jeon <dogdriip@gmail.com>
2020-05-09 01:45:55 +0900

    mod activity_my_page.xml

--- giggle_android/app/src/main/java/com/giggle/prototype/MyPageActivity.kt
+++ giggle_android/app/src/main/java/com/giggle/prototype/MyPageActivity.kt
@@ -10,11 +10,17 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 
 class MyPageActivity : AppCompatActivity() {
-
+    var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+
+        auth = FirebaseAuth.getInstance()
+        Toast.makeText(this, auth?.currentUser?.displayName, Toast.LENGTH_SHORT).show()
+        // TODO: here
+
     }
 }
