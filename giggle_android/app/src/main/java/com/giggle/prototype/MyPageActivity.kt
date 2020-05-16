a083ee78eb3b3b45c7a8678ebbd35d7624edd4a5
Hyunseung Jeon <dogdriip@gmail.com>
2020-05-17 04:15:54 +0900

    마이페이지 프래그먼트 작업
    - 마이페이지 프래그먼트에서 user 정보 가져오기
    - 마이페이지 화면 레이아웃 변경 (text center align), 아이콘 수정
    - MyPageActivity.kt, activity_my_page.xml - 사용하지 않을 예정

--- giggle_android/app/src/main/java/com/giggle/prototype/MyPageActivity.kt
+++ giggle_android/app/src/main/java/com/giggle/prototype/MyPageActivity.kt
@@ -9,19 +9,13 @@ import android.provider.MediaStore
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
-import com.google.firebase.auth.FirebaseAuth
 
 class MyPageActivity : AppCompatActivity() {
-    var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
 
-        auth = FirebaseAuth.getInstance()
-        Toast.makeText(this, auth?.currentUser?.displayName, Toast.LENGTH_SHORT).show()
-        // TODO: here
-
+        // FIXME: go to MypageFragment.kt, fragment_mypage.xml
     }
 
 }
--- giggle_android/app/src/main/java/com/giggle/prototype/MypageFragment.kt
+++ giggle_android/app/src/main/java/com/giggle/prototype/MypageFragment.kt
@@ -6,18 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_mypage.*
 
 class MypageFragment : Fragment() {
+    private val user = FirebaseAuth.getInstance().currentUser
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_mypage, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_mypage, container, false)
+    }
 
     companion object {
         fun newInstance(): MypageFragment = MypageFragment()
     }
 
+    override fun onViewCreated(view:View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val name = user?.displayName
+        val email = user?.email
+        val photoUrl = user?.photoUrl
+        // val emailVerified = user.isEmailVerified
+        // val uid = user.uid
+
+        nameText.text = name
+        emailText.text = email
+    }
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
-
     }
 }
