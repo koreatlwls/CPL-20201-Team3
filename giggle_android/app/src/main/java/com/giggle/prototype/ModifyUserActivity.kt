7383f0acb52a9f5c22552e9e110b9b57b00f0282
Hyunseung Jeon <dogdriip@gmail.com>
2020-05-17 17:23:45 +0900

    회원정보 수정 액티비티 작업, 프로필 사진 작업
    - 회원정보 수정 거의 완료
    - 마이페이지에서 프로필 사진 가져오기 완료

--- giggle_android/app/src/main/java/com/giggle/prototype/ModifyUserActivity.kt
+++ giggle_android/app/src/main/java/com/giggle/prototype/ModifyUserActivity.kt
@@ -1,13 +1,17 @@
 package com.giggle.prototype
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.android.synthetic.main.activity_modify_user.*
 
 class ModifyUserActivity : AppCompatActivity() {
     private val user = FirebaseAuth.getInstance().currentUser
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,24 +22,55 @@ class ModifyUserActivity : AppCompatActivity() {
         // get user's info
         val name = user?.displayName
         val email = user?.email
-        val photoUrl = user?.photoUrl
 
-        // TODO: see docs/auth/web/manage-users, split into two parts, construct linearlayout
+        // TODO: see docs/auth/android/manage-users, split into two parts, construct linearlayout
 
         // initialize fields
         NameText.setText(name)
-        EmailText.setText(name)
+        EmailText.setText(email)
         // PasswordText.setText("")
 
 
 
         ModifyUserOK.setOnClickListener {
             // TODO: work on updateProfile, updateEmail, updatePassword
+            // get fields
+            val nameInput = NameText.text.toString()
+            val emailInput = EmailText.text.toString()
+            val passwordInput = PasswordText.text.toString()
 
+
+            // updateProfile - name, photo
+            val profileUpdates = UserProfileChangeRequest.Builder()
+                .setDisplayName(name)
+                // .setPhotoUri(Uri.parse("https://example.com/"))
+                .build()
+            user?.updateProfile(profileUpdates)
+                ?.addOnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Toast.makeText(this, "updateProfile에서 문제가 발생했습니다.", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+            // updateEmail
+            user?.updateEmail(emailInput)
+                ?.addOnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Toast.makeText(this, "updateEmail에서 문제가 발생했습니다.", Toast.LENGTH_LONG).show()
+                    }
+                }
 
             // if password is not empty, update password too
+            if (passwordInput !== "") {
+                user?.updatePassword(passwordInput)
+                    ?.addOnCompleteListener { task ->
+                        if (!task.isSuccessful) {
+                            Toast.makeText(this, "updatePassword에서 문제가 발생했습니다.", Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }
 
-
+            // completed
             // Toast.makeText(this, "회원정보 수정이 완료되었습니다.", Toast.LENGTH_LONG).show()
         }
 
--- giggle_android/app/src/main/java/com/giggle/prototype/MypageFragment.kt
+++ giggle_android/app/src/main/java/com/giggle/prototype/MypageFragment.kt
@@ -2,16 +2,22 @@ package com.giggle.prototype
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.fragment_mypage.*
 
 class MypageFragment : Fragment() {
     private val user = FirebaseAuth.getInstance().currentUser
+    private val storage = FirebaseStorage.getInstance()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_mypage, container, false)
@@ -28,13 +34,26 @@ class MypageFragment : Fragment() {
 
         val name = user?.displayName
         val email = user?.email
-        val photoUrl = user?.photoUrl
-        // val emailVerified = user.isEmailVerified
-        // val uid = user.uid
+        val uid = user?.uid
 
+        // get profile pic
+        var storageRef = storage.reference
+        var imageRef = storageRef.child("profile_image/${uid}.png")
+        imageRef.downloadUrl.addOnSuccessListener {
+            // Got the download URL for 'users/me/profile.png'
+            Glide.with(this)
+                .load(it)
+                .centerCrop()
+                .into(img_user)
+        }.addOnFailureListener {
+            Toast.makeText(activity, "프로필 사진이 없습니다.", Toast.LENGTH_LONG).show()
+        }
+
+        // set fields
         nameText.text = name
         emailText.text = email
 
+        // button listeners
         btn_parttime.setOnClickListener {
             // Toast.makeText(activity,"아르바이트 신청", Toast.LENGTH_LONG).show()
 
