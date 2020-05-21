7c8ebf79e8ed2a766fb9322201d1182f05ff86e9
Hyunseung Jeon <dogdriip@gmail.com>
2020-05-21 19:46:20 +0900

    구직자 광고목록 Fragment 작업 (2)
    - 검색기능 구현 완료
    - RecyclerView에 결과 표시 구현중

--- giggle_android/app/src/main/java/com/giggle/prototype/ListADFragment.kt
+++ giggle_android/app/src/main/java/com/giggle/prototype/ListADFragment.kt
@@ -8,14 +8,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.fragment_list_ad.*
 import kotlinx.android.synthetic.main.fragment_mypage.*
 
 class ListADFragment : Fragment() {
     // private val user = FirebaseAuth.getInstance().currentUser
-    // private val storage = FirebaseStorage.getInstance()
+    private val db = FirebaseFirestore.getInstance()
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_list_ad, container, false)
@@ -28,11 +32,49 @@ class ListADFragment : Fragment() {
     override fun onViewCreated(view:View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        linearLayoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = linearLayoutManager
 
 
 
 
 
+
+        searchBtn.setOnClickListener{
+            if (searchText.text.isEmpty()) {
+                Toast.makeText(context, "검색어를 입력해주세요.", Toast.LENGTH_LONG).show()
+            } else {
+                val text = searchText.text.toString()
+
+                db.collection("jobads")
+                    .get()
+                    .addOnSuccessListener { result ->
+                        for (document in result) {
+                            println(document.data)
+                            // Toast.makeText(context, "${}", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Toast.makeText(context, "데이터를 가져오는 데 실패했습니다.", Toast.LENGTH_LONG).show()
+                    }
+
+                db.collection("jobads")
+                    // .whereArrayContains("shopposition", text)
+                    .whereEqualTo("shopposition", text)
+                    .get()
+                    .addOnSuccessListener { result ->
+                        for (document in result) {
+                            println(document.data["shopname"])
+                            // Toast.makeText(context, "${}", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Toast.makeText(context, "데이터를 가져오는 데 실패했습니다.", Toast.LENGTH_LONG).show()
+                    }
+
+
+            }
+        }
 
     }
 
---
+++ giggle_android/app/src/main/java/com/giggle/prototype/RecyclerAdapter.kt
@@ -0,0 +1,9 @@
+package com.giggle.prototype
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter() {
+
+}
