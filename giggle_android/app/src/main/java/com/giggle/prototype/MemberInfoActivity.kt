b45a7b369eb4bf5092f2d0e58cf161f4c0e8d0b2
koreatlwls <61337202+koreatlwls@users.noreply.github.com>
2020-05-23 13:11:50 +0900

    Android14
    Android 14
    [upload 1 구현 기능]
    1. 로그인
    2. 회원가입 (이메일 인증)
    3. 비밀번호 재설정 (이메일)
    [upload 2 구현 기능]
    1. 현재 위치 표시 (Google Maps SDK)
    2. 주소 검색 결과를 마커로 표시 (Google Maps SDK)
    [upload 3]
    1. UI를 탭 뷰로 전환
    [upload 4]
    1. 광고 클래스 추가
    2. 광고 정보를 저장하기 위한 데이터베이스 추가 (Firestore)
    3. Cloud Storage를 통하여 이미지 파일 저장
    [upload 5]
    1. 광고 액티비티를 프래그먼트 파일로 변환
    2. 광고 레이아웃을 프래그먼트 파일로 변환
    [upload 6]
    1. 마이페이지 프래그먼트 작업
    2. 회원정보 수정 액티비티 작업
    3. 마이페이지 구성, 프로필 사진 작업
    [upload 7]
    1. 구글맵 프래그먼트 이식
    2. 장소 검색 기능 이식
    3. 현재 위치 검색 기능 이식
    4. 광고 등록 fragment에 구글맵 등록
    [upload 8]
    1. 광고 등록 레이아웃 수정
    2. 입력값 검증 에러 수정
    [upload 9]
    1. 구인자/구직자 화면 전환버튼 생성 (전환가능)
    2. 구인자 로그인화면, 구인자 마이페이지
    3. 구직자 로그인화면, 구직자 마이페이지
    [upload 10]
    1.  상점 위치를 주소 검색 결과값으로 등록 기능 구현
    2. 상점 위치를 현재 위치 버튼 클릭 결과값으로 등록 기능 구현
    3. Layout 수정
    [upload 11]
    1. 구직자 광고목록 Fragment 작업
    2. 구직자 광고목록 - 검색기능
    [upload 12]
    1. 회원가입 필드 수정
    2. 마이페이지 - 프로필 사진 storage 작업 수정
    3. 광고등록 alertdialog 추가
    [upload 13]
    1. 회원가입,비번재설정 레이아웃 수정
    2. 마이페이지_구인자 정보등록 구현 및 member DB 생성
    [upload 14]
    1. 마이페이지_진행중인 채용 리스트뷰 구현
    2. member DB 전화번호 추가

--- giggle_android/app/src/main/java/com/giggle/prototype/MemberInfoActivity.kt
+++ giggle_android/app/src/main/java/com/giggle/prototype/MemberInfoActivity.kt
@@ -19,7 +19,11 @@ class MemberInfoActivity : AppCompatActivity() {
             var sex = ""
             var age = 0
             val position = edMemPosition.text.toString()
-
+            var number = ""
+            if(edMemPhone1.text.isNotEmpty()&&edMemPhone2.text.isNotEmpty()&&edMemPhone3.text.isNotEmpty()){
+                number = edMemPhone1.text.toString()+"-"+edMemPhone2.text.toString()+"-"+edMemPhone3.text.toString()
+                println(number)
+            }
             if(edMemAge.text.isNotEmpty()){
                 age = Integer.parseInt(edMemAge.text.toString())
             }
@@ -29,10 +33,13 @@ class MemberInfoActivity : AppCompatActivity() {
             else if(radioWomen.isChecked){
                 sex = radioWomen.text.toString()
             }
-            if(name.isEmpty()||position.isEmpty()||age<1||sex.isEmpty()){
+            if(name.isEmpty()||position.isEmpty()||age<1||sex.isEmpty()||number.isEmpty()){
                 edMemName.setBackgroundResource(R.drawable.red_edittext)
                 edMemAge.setBackgroundResource(R.drawable.red_edittext)
                 edMemPosition.setBackgroundResource(R.drawable.red_edittext)
+                edMemPhone1.setBackgroundResource(R.drawable.red_edittext)
+                edMemPhone2.setBackgroundResource(R.drawable.red_edittext)
+                edMemPhone3.setBackgroundResource(R.drawable.red_edittext)
                 Toast.makeText(this, "모든 항목을 채워주세요.", Toast.LENGTH_LONG).show()
             }
             if(name.isNotEmpty()&&position.isNotEmpty()&&age>0&&sex.isNotEmpty()){
@@ -42,7 +49,7 @@ class MemberInfoActivity : AppCompatActivity() {
                 builder.setPositiveButton("확인"){_,_->
                     val user = FirebaseAuth.getInstance().currentUser
                     val db = FirebaseFirestore.getInstance()
-                    val member = members(name,age,sex,position, user?.uid)
+                    val member = members(name,age,sex,position, user?.uid,number)
                     if (user != null) {
                         db.collection("members").document(user.uid).set(member)
                     } //DB에 uid기준으로 멤버정보 저장
@@ -63,5 +70,14 @@ class MemberInfoActivity : AppCompatActivity() {
         edMemPosition.setText(null)
         radioMan.setChecked(false)
         radioWomen.setChecked(false)
+        edMemPhone1.setText(null)
+        edMemPhone2.setText(null)
+        edMemPhone3.setText(null)
+        edMemName.setBackgroundResource(R.drawable.border_dark)
+        edMemAge.setBackgroundResource(R.drawable.border_dark)
+        edMemPosition.setBackgroundResource(R.drawable.border_dark)
+        edMemPhone1.setBackgroundResource(R.drawable.border_dark)
+        edMemPhone2.setBackgroundResource(R.drawable.border_dark)
+        edMemPhone3.setBackgroundResource(R.drawable.border_dark)
     }
 }
