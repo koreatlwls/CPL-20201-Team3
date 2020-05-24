98b639403aa03b0d871450f3536a6038915f51aa
Park Jong Ho <57793298+HoJongPARK@users.noreply.github.com>
2020-05-24 18:44:41 +0900

    Android Layout4
    [Layout1]
    1. Bottom Navigation View Color 설정
    2. Title bar 제거
    [Layout2]
    1. 광고 등록 fragment 레이아웃 수정
    [Layout3]
    1. 광고 등록 floating action button 구현
    2. 광고 등록 fragment 레이아웃 수정
    3. editText->textinputlayout으로 수정
    [Layout4]
    1. 마이 페이지 fragment 레이아웃 수정
    2. 마이페이지 cardview 가장 최근 광고 연결

--- giggle_android/app/src/main/java/com/giggle/prototype/MypageFragment.kt
+++ giggle_android/app/src/main/java/com/giggle/prototype/MypageFragment.kt
@@ -10,8 +10,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.fragment_mypage.*
 
@@ -20,6 +22,12 @@ class MypageFragment : Fragment() {
     private val storage = FirebaseStorage.getInstance()
     val PICK_IMAGE_FROM_ALBUM = 0
     var photoUri: Uri? =null //프로필 사진
+    var shopname ="0"
+    var shopphoto ="0"
+    var shopposition="0"
+    val db = FirebaseFirestore.getInstance()
+    var i:Int=0
+    private lateinit var mContext:FragmentActivity
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_mypage, container, false)
     }
@@ -29,15 +37,41 @@ class MypageFragment : Fragment() {
     }
 
     // TODO: icon size on fragment_mypage.xml
-
+    override fun onAttach(activity: Activity) {
+        mContext = activity as FragmentActivity
+        super.onAttach(activity)
+    }
     override fun onViewCreated(view:View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val email = user?.email
         val uid = user?.uid
+        var storageRef = storage.reference
+        db.collection("jobads")
+            .whereEqualTo("uid", uid)//내가 올린 광고인지 체크
+            .whereEqualTo("state",0)//진행중인 광고인지 체크
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    if (i == 0) {
+                        shopname = document.data["shopname"].toString() //가게이름
+                        shopposition = document.data["shopposition"].toString()
+                        shopphoto = document.data["photouri"].toString() //이미지
+                        i = 1
+                        Glide.with(this)
+                            .load(shopphoto)
+                            .centerCrop()
+                            .into(imageview_main_image2)
+                        txshopname.setText(shopname)
+                        txshopposition.setText(shopposition)
+                    }
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(mContext, "데이터를 가져오는 데 실패했습니다.", Toast.LENGTH_LONG).show()
+            }
 
         // get profile pic
-        var storageRef = storage.reference
+
         var imageRef = storageRef.child("profile_image/${uid}.png")
         imageRef.downloadUrl.addOnSuccessListener {
             // Got the download URL for 'users/me/profile.png'
