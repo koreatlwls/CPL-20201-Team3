4e4100cd26bc7704671d42dc40362a6bdefd0b62
Park Jong Ho <57793298+HoJongPARK@users.noreply.github.com>
2020-05-24 15:18:19 +0900

    Android Layout4
    [Layout1]
    1. Bottom Navigation View Color 설정
    2. Title bar 제거
    [Layout2]
    1. 광고 등록 fragment 레이아웃 수정
    [Layout3]
    1. 광고 등록 floating action button 구현
    2. 광고 등록 fragment 레이아웃 수정
    3. editText->textinputlayout으로 수정
    [Layout4]
    1. 마이 페이지 fragment 레이아웃 수정

--- giggle_android/app/src/main/java/com/giggle/prototype/SignInActivity.kt
+++ giggle_android/app/src/main/java/com/giggle/prototype/SignInActivity.kt
@@ -76,57 +76,12 @@ class SignInActivity : AppCompatActivity() {
 
         val bottomNavigationView : BottomNavigationView = findViewById(R.id.bottom_nav)
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-
-
-
     }
-
-
-    /*
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        when (requestCode) {
-            REQUEST_ACCESS_FINE_LOCATION -> {
-                if (grantResults.isNotEmpty() && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED
-                ) {
-                    // 권한이 승인 됐다면
-                    addLocationListener()
-                } else {
-                    // 권한이 거부 됐다면
-                    toast("권한 거부 됨")
-                }
-                return
-            }
-        }
-    }
-
-*/
     override fun onResume() {//잠깐 쉴 때
         super.onResume()
     }
-
     override fun onPause() {
         super.onPause()
     }
-/*
-    fun removeLocationListener() {
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
-    }//어플이 종료되면 지도 요청 해제
-
-    @SuppressLint("MissingPermission")
-    //위험 권한 사용 시 요청 코드가 호출 되어야 하는데
-    //없어서 발생됨. 요청 코드는 따로 처리 했음
-    fun addLocationListener() {
-        fusedLocationProviderClient.requestLocationUpdates(
-            locationRequest,
-            locationCallback, null
-        ) //위치 권한을 요청해야 함.
-        //액티비티가 잠깐 쉴 때
-        //자신의 위치를 확인하고 갱신된 정보를 요청
-
-    }
- */
 }
 
